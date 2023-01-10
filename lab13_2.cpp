f542cec8be8d17c37bf47e0944472ba05068aa0b
Holytis <holytis123@gmail.com>
2023-01-10 15:30:28 +0700

    sed laew

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x , int y)
+{
+    if (x%y==0){
+        return y ;
+    }else return gcd(y , x%y);
+}
+
 int main(){
     int x, y;
     cin >> x >> y;
